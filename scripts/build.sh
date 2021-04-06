3430f5bb0e12b7e4d33f89294f1c4615072156e3
Grant Gryczan <grantgryczan@gmail.com>
2021-04-06 10:05:14 -0400

    Parse TS without transformation

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 rm -r out/*
-npx babel modules --out-dir out/modules --extensions .js,.ts --keep-file-extension --copy-files &
-npx babel datapacks --out-dir out/datapacks --extensions .js,.ts --keep-file-extension --copy-files &
+npx babel modules --out-dir out/modules --extensions .js,.ts --keep-file-extension --copy-files --source-maps &
+npx babel datapacks --out-dir out/datapacks --extensions .js,.ts --keep-file-extension --copy-files --source-maps &
 wait
 npx sand build out/$1
--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,5 +1,5 @@
 rm -r out/*
-npx babel modules --out-dir out/modules --extensions .js,.ts --keep-file-extension --copy-files --watch &
-npx babel datapacks --out-dir out/datapacks --extensions .js,.ts --keep-file-extension --copy-files --watch &
+npx babel modules --out-dir out/modules --extensions .js,.ts --keep-file-extension --copy-files --source-maps --watch &
+npx babel datapacks --out-dir out/datapacks --extensions .js,.ts --keep-file-extension --copy-files --source-maps --watch &
 npx sand watch out/$1 &
 wait
