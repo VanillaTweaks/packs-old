bb442d8035272b4d974e7ecdf3e0ea34e2636871
Grant Gryczan <grantgryczan@gmail.com>
2021-12-05 04:22:39 -0500

    `modules` -> `lib`

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,5 +1,5 @@
 rm -r out/*
-npx babel modules --out-dir out/modules --extensions .js,.ts --keep-file-extension --copy-files --source-maps --watch &
+npx babel lib --out-dir out/lib --extensions .js,.ts --keep-file-extension --copy-files --source-maps --watch &
 npx babel datapacks --out-dir out/datapacks --extensions .js,.ts --keep-file-extension --copy-files --source-maps --watch &
 npx sand watch out/$1 &
 wait
