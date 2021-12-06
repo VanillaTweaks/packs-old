5a4257c6282cb790599186830f87637c275ac49e
Grant Gryczan <grantgryczan@gmail.com>
2021-12-05 23:47:50 -0500

    Implement Lantern Load

---
+++ lib/datapacks/lanternLoad.ts
@@ -0,0 +1,27 @@
+// A faithful implementation of https://github.com/LanternMC/load for Vanilla Tweaks.
+
+import { BasePath, scoreboard } from 'sandstone';
+import withVT from 'lib/datapacks/withVT';
+
+const lanternLoad = withVT(
+	BasePath({ namespace: 'load' })
+);
+
+const privateInitTag = lanternLoad.Tag('functions', '_private/init', [
+	lanternLoad.MCFunction('_private/init', () => {
+		// Reset scoreboards so packs can set values accurate for current load.
+		scoreboard.objectives.add('load.status', 'dummy');
+		scoreboard.players.reset('*', 'load.status');
+	})
+]);
+
+export const loadTag = lanternLoad.Tag('functions', 'load');
+
+lanternLoad.Tag('functions', '_private/load', [
+	privateInitTag,
+	{ id: lanternLoad`pre_load`, required: false },
+	{ id: loadTag, required: false },
+	{ id: lanternLoad`post_load`, required: false }
+], {
+	runOnLoad: true
+});
