7f00535ccf7a95246b786faf68635b30ee5fad59
Grant Gryczan <grantgryczan@gmail.com>
2021-12-06 04:20:50 -0500

    Remove unnecessary variable

--- lib/datapacks/lanternLoad.ts
+++ lib/datapacks/lanternLoad.ts
@@ -7,18 +7,16 @@ const lanternLoad = withVT(
 	BasePath({ namespace: 'load' })
 );
 
-const privateInitTag = lanternLoad.Tag('functions', '_private/init', [
-	lanternLoad.MCFunction('_private/init', () => {
-		// Reset scoreboards so packs can set values accurate for current load.
-		scoreboard.objectives.add('load.status', 'dummy');
-		scoreboard.players.reset('*', 'load.status');
-	})
-]);
-
 export const loadTag = lanternLoad.Tag('functions', 'load');
 
 lanternLoad.Tag('functions', '_private/load', [
-	privateInitTag,
+	lanternLoad.Tag('functions', '_private/init', [
+		lanternLoad.MCFunction('_private/init', () => {
+			// Reset scoreboards so packs can set values accurate for current load.
+			scoreboard.objectives.add('load.status', 'dummy');
+			scoreboard.players.reset('*', 'load.status');
+		})
+	]),
 	{ id: lanternLoad`pre_load`, required: false },
 	{ id: loadTag, required: false },
 	{ id: lanternLoad`post_load`, required: false }
