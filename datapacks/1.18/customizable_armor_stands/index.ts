01a27313133a84b411833df4f6a953ef72a9c671
Grant Gryczan <grantgryczan@gmail.com>
2021-12-13 00:41:39 -0500

    Remove old code

--- datapacks/1.18/customizable_armor_stands/index.ts
+++ datapacks/1.18/customizable_armor_stands/index.ts
@@ -1,6 +1,4 @@
 import pack, { pack_ } from 'lib/datapacks/pack';
-import { advancement, data, execute, MCFunction, NBT, schedule, tag } from 'sandstone';
-import FunctionalAdvancement from 'lib/datapacks/FunctionalAdvancement';
 import setMetaAdvancements from 'lib/datapacks/pack/setMetaAdvancements';
 import setConfigFunction from 'lib/datapacks/setConfigFunction';
 import onUninstall from 'lib/datapacks/onUninstall';
@@ -47,41 +45,4 @@ onUninstall(pack, () => {
 
 setConfigFunction(pack, () => {
 
-});
-
-const activateGrave = MCFunction(pack_`activate_grave`, () => {
-	advancement.revoke('@s').only(interactWithGraveAdvancement);
-	tag('@s').add('graves.subject');
-
-	// Get the ID of the activated grave.
-	execute.store.result.score('#activated', 'graves.dummy').run.data.get.entity('@s', 'Inventory[{tag:{gravesData:{activator:1b}}}].tag.gravesData.id');
-
-	// Check which grave matches that ID and tag it as activated.
-	execute.as('@e[type=minecraft:armor_stand,tag=graves.hitbox]').run(pack_`check_hitbox`, () => {
-		execute.store.result.score('@s', 'graves.id').run.data.get.entity('@s', 'HandItems[1].tag.gravesData.id');
-		execute.if.score('@s', 'graves.id', '=', '#activated', 'graves.dummy').run.tag('@s').add('graves.activated');
-	});
-
-	// If grave robbing is disabled and they didn't use a grave key,
-	execute.if.score('#robbing', 'graves.config', 'matches', 0).unless.data.entity('@e[type=minecraft:armor_stand,tag=graves.activated,limit=1]', 'ArmorItems[{tag:{gravesKey:1b}}]').run(pack_`check_owner`, () => {
-		data.modify.storage('graves:storage', 'temp').set.from.entity('@s', 'UUID');
-		execute.store.success.score('#success', 'graves.dummy').run.data.modify.storage('graves:storage', 'temp').set.from.entity('@e[type=minecraft:armor_stand,tag=graves.activated,limit=1]', 'HandItems[1].tag.gravesData.uuid');
-	});
-});
-
-const interactWithGraveAdvancement = FunctionalAdvancement(pack`interact_with_grave`, {
-	trigger: 'minecraft:player_interacted_with_entity',
-	conditions: {
-		entity: {
-			type: 'minecraft:armor_stand',
-			nbt: NBT.stringify({
-				Tags: ['graves.hitbox']
-			})
-		}
-	}
-}, 'interact_with_grave', () => {
-	activateGrave();
-	schedule.function(pack_`activate_graves`, () => {
-		execute.as(`@a[advancements={${interactWithGraveAdvancement}=true}]`).at('@s').run(activateGrave);
-	}, '2t', 'append');
 });
