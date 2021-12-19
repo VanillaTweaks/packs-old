9b43e228734a20a8d768d446860e0e6d66692dba
Grant Gryczan <grantgryczan@gmail.com>
2021-12-18 20:09:02 -0500

    `pageHead` -> `page`

---
+++ lib/datapacks/textComponents/page.ts
@@ -0,0 +1,37 @@
+import * as meta from 'lib/meta';
+import horizontalBar from 'lib/datapacks/textComponents/horizontalBar';
+import type { JSONTextComponent } from 'sandstone';
+import { tellraw } from 'sandstone';
+import minifyComponent from 'lib/datapacks/textComponents/minifyComponent';
+
+/** Runs `tellraw` commands to display the top and bottom of a page UI to `@s`. */
+const page = (
+	options: {
+		/** Content placed directly before the title, minified automatically. Put `padding` here. */
+		before?: JSONTextComponent,
+		/** The part of the center text before the slash, minified automatically. Defaults to the name of the pack. */
+		title?: string,
+		/** The part of the center text after the slash, minified automatically. */
+		subtitle: string,
+		/** Content placed directly after the subtitle, minified automatically. */
+		after?: JSONTextComponent
+	},
+	content: () => void
+) => {
+	horizontalBar();
+	tellraw('@s', minifyComponent([
+		'',
+		options.before || '',
+		options.title || meta.title,
+		{ text: ' / ', color: 'gray' },
+		options.subtitle,
+		options.after || ''
+	]));
+	horizontalBar();
+
+	content();
+
+	horizontalBar();
+};
+
+export default page;
