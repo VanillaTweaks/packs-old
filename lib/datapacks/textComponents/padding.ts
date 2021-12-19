a3589d25dbc03cba22c724fbf0db2e8d92eca0d1
Grant Gryczan <grantgryczan@gmail.com>
2021-12-19 00:07:49 -0500

    Improve `padding` comments

--- lib/datapacks/textComponents/padding.ts
+++ lib/datapacks/textComponents/padding.ts
@@ -1,18 +1,30 @@
 import minify from 'lib/datapacks/textComponents/minify';
 
-/** Returns a `JSONTextComponent` of a combination of plain and bold spaces to achieve a specified width in pixels. */
+/**
+ * Returns a `JSONTextComponent` of a combination of plain and bold spaces to achieve a specified width in in-game pixels.
+ *
+ * The width cannot be 1, 2, 3, 6, or 11.
+ */
 const padding = (
-	/** The number of pixels to generate spaces for. */
-	pixels: number
+	/**
+	 * The number of in-game pixels to generate spaces for.
+	 *
+	 * Cannot be 1, 2, 3, 6, or 11.
+	 */
+	width: number
 ) => {
-	if (!Number.isInteger(pixels) || (
-		pixels < 12 && ![4, 5, 8, 9, 10].includes(pixels)
-	)) {
-		throw TypeError(`The number of pixels cannot be ${pixels}.`);
+	if (width === 0) {
+		return '';
 	}
 
-	const boldSpaces = pixels % 4;
-	const plainSpaces = Math.floor(pixels / 4) - boldSpaces;
+	if (!Number.isInteger(width) || (
+		width < 12 && ![4, 5, 8, 9, 10].includes(width)
+	)) {
+		throw TypeError(`The width cannot be ${width}.`);
+	}
+
+	const boldSpaces = width % 4;
+	const plainSpaces = Math.floor(width / 4) - boldSpaces;
 
 	return minify([
 		' '.repeat(plainSpaces),
