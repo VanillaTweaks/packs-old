d06e4dd0fe93cbd29a355b7eaaf08643ecba0d8b
Grant Gryczan <grantgryczan@gmail.com>
2021-12-20 06:14:19 -0500

    Implement `center`

---
+++ lib/datapacks/textComponents/center.ts
@@ -0,0 +1,41 @@
+import type { JSONTextComponent } from 'sandstone';
+import split from 'lib/datapacks/textComponents/split';
+import getWidth from 'lib/datapacks/textComponents/getWidth';
+import padding from 'lib/datapacks/textComponents/padding';
+import minify from 'lib/datapacks/textComponents/minify';
+import join from 'lib/datapacks/textComponents/join';
+import { BOOK_WIDTH } from 'lib/datapacks/textComponents/container';
+
+/**
+ * Centers a text component by adding padding to the left of it, automatically minified.
+ *
+ * Assumes all arrays in the inputted component have elements which shouldn't inherit special formatting from the first element, so it isn't necessary to avoid special formatting on the first element of any inputted array.
+ */
+const center = (
+	component: JSONTextComponent,
+	/** The width of the container to be centered in. Defaults to `BOOK_WIDTH`. */
+	containerWidth = BOOK_WIDTH
+) => (
+	join(
+		split(component, '\n').map(componentLine => {
+			const width = getWidth(componentLine);
+
+			if (width === 0) {
+				// If the line is empty, just leave it empty rather than adding useless padding.
+				return '';
+			}
+
+			if (width > containerWidth) {
+				throw new TypeError('The width of a line cannot exceed the width of the container.');
+			}
+
+			return minify([
+				padding((containerWidth - width) / 2),
+				componentLine
+			]);
+		}),
+		'\n'
+	)
+);
+
+export default center;
---
+++ lib/datapacks/textComponents/container.ts
@@ -0,0 +1,2 @@
+/** The effective maximum width of a line of text in a book in in-game pixels. */
+export const BOOK_WIDTH = 114;
