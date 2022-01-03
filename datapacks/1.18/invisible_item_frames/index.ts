556f3e994406e4ef0d770542e30f9e70fcc297b9
Grant Gryczan <grantgryczan@gmail.com>
2022-01-03 17:06:27 -0500

    Invisible Item Frames boilerplate

---
+++ datapacks/1.18/invisible_item_frames/index.ts
@@ -0,0 +1,23 @@
+import setMetaAdvancements from 'lib/datapacks/setMetaAdvancements';
+
+setMetaAdvancements({
+	root: {
+		icon: {
+			item: 'minecraft:item_frame'
+		},
+		titlePadding: 36,
+		description: [
+			'',
+			{ text: 'Shear an item frame to make it invisible.', color: 'gold' }
+		]
+	},
+	usage: {
+		titlePadding: 40,
+		description: [
+			''
+		]
+	},
+	opUsage: {
+		titlePadding: 56
+	}
+});
