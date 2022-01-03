f6b5ad764cc7517b5c28854a3395f04f8f43c41a
Grant Gryczan <grantgryczan@gmail.com>
2022-01-02 22:12:57 -0500

    Reduce compiled file size

--- lib/datapacks/faultChecking/vtNotUninstalled.ts
+++ lib/datapacks/faultChecking/vtNotUninstalled.ts
@@ -1,30 +1,29 @@
 import type { PredicateCondition } from 'sandstone';
-import { Predicate } from 'sandstone';
 import loadStatusOf from 'lib/datapacks/lanternLoad/loadStatusOf';
 import vt from 'lib/datapacks/vt';
 
 const $vtLoadStatus = loadStatusOf(vt);
 
-/** A `PredicateCondition` that ensures VT is not uninstalled, since otherwise an advancement's reward function could run even after everything is supposed to be uninstalled. */
+/**
+ * A `PredicateCondition` that ensures VT is not uninstalled, since otherwise an advancement's reward function could run even after everything is supposed to be uninstalled.
+ *
+ * ⚠️ Only for use within `faultChecking` modules.
+ */
 const vtNotUninstalled: PredicateCondition = {
-	condition: 'minecraft:reference',
-	name: Predicate(vt`not_uninstalled`, {
-		condition: 'minecraft:inverted',
-		term: {
-			condition: 'minecraft:value_check',
-			value: {
-				type: 'minecraft:score',
-				target: {
-					type: 'minecraft:fixed',
-					name: $vtLoadStatus.target.toString()
-				},
-				score: $vtLoadStatus.objective
+	condition: 'minecraft:inverted',
+	term: {
+		condition: 'minecraft:value_check',
+		value: {
+			type: 'minecraft:score',
+			target: {
+				type: 'minecraft:fixed',
+				name: $vtLoadStatus.target.toString()
 			},
-			range: -1
-		// TODO: Remove `as any`.
-		} as any
-	// TODO: Remove `.toString()`.
-	}).toString()
+			score: $vtLoadStatus.objective
+		},
+		range: -1
+	// TODO: Remove `as any`.
+	} as any
 };
 
 export default vtNotUninstalled;
