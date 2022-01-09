be6ae62132af7a5fc53f710e8c761d620fc4ad92
Grant Gryczan <grantgryczan@gmail.com>
2022-01-09 04:43:20 -0500

    Disallow paths in the pack's namespace

--- lib/datapacks/pack.ts
+++ lib/datapacks/pack.ts
@@ -4,4 +4,8 @@ import { title, namespace, version } from 'lib/meta';
 /** The `ResourceLocation` for the data pack's namespace. */
 const pack = ResourceLocation(namespace, { title, version });
 
+if (pack.path !== undefined) {
+	throw new TypeError('The pack\'s namespace is invalid.');
+}
+
 export default pack;
