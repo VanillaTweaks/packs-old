e360fd477f8f691c8f21a0a4ec65694e540d4400
Grant Gryczan <grantgryczan@gmail.com>
2022-01-09 04:58:34 -0500

    Remove useless comment

--- lib/datapacks/lanternLoad/loadStatusOf.ts
+++ lib/datapacks/lanternLoad/loadStatusOf.ts
@@ -2,10 +2,7 @@ import type { ResourceLocationInstance } from 'lib/datapacks/ResourceLocation';
 import { loadStatus } from 'lib/datapacks/lanternLoad';
 
 /** Gets the `loadStatus` score of a `ResourceLocation`. */
-const loadStatusOf = (
-	/** The `ResourceLocation` to get the `loadStatus` score of. */
-	resourceLocation: ResourceLocationInstance
-) => (
+const loadStatusOf = (resourceLocation: ResourceLocationInstance) => (
 	loadStatus(
 		'$' + resourceLocation.namespace + (
 			resourceLocation.path
