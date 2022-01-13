408c249c430397cfb167727aed8dbbe908b5ea74
Grant Gryczan <grantgryczan@gmail.com>
2022-01-13 03:48:05 -0500

    Fix unnecessary `''` being inserted by `minify`

--- lib/datapacks/textComponents/minify/disableInheritanceIfNecessary.ts
+++ lib/datapacks/textComponents/minify/disableInheritanceIfNecessary.ts
@@ -1,7 +1,8 @@
 import type { MinifyOutputArray } from 'lib/datapacks/textComponents/minify';
 import { notWhitespace, notLineBreaks } from 'lib/datapacks/textComponents/minify/regex';
 import getHeritableProperties from 'lib/datapacks/textComponents/getHeritableProperties';
-import type { HeritableKey } from 'lib/datapacks/textComponents/heritableKeys';
+import type { HeritableKey, WhitespaceAffectedByKey } from 'lib/datapacks/textComponents/heritableKeys';
+import { whitespaceAffectedByKeys } from 'lib/datapacks/textComponents/heritableKeys';
 import whitespaceUnaffectedBy from 'lib/datapacks/textComponents/whitespaceUnaffectedBy';
 
 /**
@@ -12,10 +13,37 @@ import whitespaceUnaffectedBy from 'lib/datapacks/textComponents/whitespaceUnaff
 const disableInheritanceIfNecessary = (output: MinifyOutputArray) => {
 	// Check if other subcomponents would inherit unwanted properties from the first subcomponent.
 	if (output.length > 1) {
+		/** The first subcomponent's heritable properties. */
 		const heritableProperties = getHeritableProperties(output[0]);
 		if (heritableProperties) {
+			/** The keys of the first subcomponent's heritable properties. */
+			let heritablePropertyKeys: HeritableKey[] | undefined;
+			/** Resolves and returns the value of `heritablePropertyKeys`. */
+			const getHeritablePropertyKeys = () => {
+				if (heritablePropertyKeys === undefined) {
+					heritablePropertyKeys = Object.keys(heritableProperties) as HeritableKey[];
+				}
+
+				return heritablePropertyKeys;
+			};
+
 			/** Whether the first subcomponent has heritable properties which affect whitespace. */
 			let heritablePropertiesAffectWhitespace: boolean | undefined;
+
+			/** The keys of the first subcomponent's heritable properties which affect whitespace. */
+			let heritablePropertyKeysWhichAffectWhitespace: WhitespaceAffectedByKey[] | undefined;
+			/** Resolves and returns the value of `heritablePropertyKeysWhichAffectWhitespace`. */
+			const getHeritablePropertyKeysWhichAffectWhitespace = () => {
+				if (heritablePropertyKeysWhichAffectWhitespace === undefined) {
+					heritablePropertyKeysWhichAffectWhitespace = getHeritablePropertyKeys().filter(
+						(key): key is WhitespaceAffectedByKey => (
+							(whitespaceAffectedByKeys as readonly string[]).includes(key)
+						)
+					);
+				}
+
+				return heritablePropertyKeysWhichAffectWhitespace;
+			};
 
 			/** Returns whether a specified string is affected by the heritable properties of the first subcomponent, or `undefined` if it depends on what formatting the string has. */
 			const isStringAffected = (string: string) => {
@@ -43,14 +71,15 @@ const disableInheritanceIfNecessary = (output: MinifyOutputArray) => {
 				output.unshift('');
 			};
 
-			let heritablePropertyKeys;
-
 			for (let i = 1; i < output.length; i++) {
 				const subcomponent = output[i];
 
 				if (typeof subcomponent === 'object') {
+					let subcomponentIsWhitespace = false;
+
 					if ('text' in subcomponent) {
-						const stringAffected = isStringAffected(subcomponent.text.toString());
+						const text = subcomponent.text.toString();
+						const stringAffected = isStringAffected(text);
 
 						if (stringAffected) {
 							preventInheritance();
@@ -60,19 +89,23 @@ const disableInheritanceIfNecessary = (output: MinifyOutputArray) => {
 						if (stringAffected === false) {
 							continue;
 						}
+
+						subcomponentIsWhitespace = !notWhitespace.test(text);
 					}
 
-					if (heritablePropertyKeys === undefined) {
-						heritablePropertyKeys = Object.keys(heritableProperties) as HeritableKey[];
-					}
+					/** All the keys of the first subcomponent which must be overwritten by this subcomponent in order for this subcomponent to remain unaffected by the inheritance from the first component. */
+					const keysWhichMustBeOverwritten = (
+						subcomponentIsWhitespace
+							? getHeritablePropertyKeysWhichAffectWhitespace()
+							: getHeritablePropertyKeys()
+					);
+					if (keysWhichMustBeOverwritten.some(key => (
+						subcomponent[key] === undefined
+					))) {
+						// A heritable property of the first subcomponent is missing from this subcomponent, so this subcomponent would inherit it.
 
-					for (const heritablePropertyKey of heritablePropertyKeys) {
-						if (subcomponent[heritablePropertyKey] === undefined) {
-							// A heritable property of the first subcomponent is missing from this subcomponent, so this subcomponent would inherit it.
-
-							preventInheritance();
-							return;
-						}
+						preventInheritance();
+						return;
 					}
 
 					continue;
