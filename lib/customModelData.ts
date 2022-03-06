6596f8b8fb811e8017ab5e9f8b8027e69d5ca315
Grant Gryczan <grantgryczan@gmail.com>
2022-03-05 23:25:31 -0500

    Improve phrasing

--- lib/customModelData.ts
+++ lib/customModelData.ts
@@ -2,7 +2,7 @@
 const CUSTOM_MODEL_DATA_BASE = 880000;
 
 /**
- * Gets an absolute `CustomModelData` value from a value between 0 and 9999 (inclusive).
+ * Gets an absolute `CustomModelData` value from a value from 0 to 9999.
  *
  * Example:
  *
@@ -14,7 +14,7 @@ const CUSTOM_MODEL_DATA_BASE = 880000;
  */
 const customModelData = (value: number) => {
 	if (!Number.isInteger(value) || value < 0 || value > 9999) {
-		throw new TypeError('The `CustomModelData` value must be an integer between 0 and 9999 (inclusive).');
+		throw new TypeError('The `CustomModelData` value must be an integer from 0 to 9999.');
 	}
 
 	return CUSTOM_MODEL_DATA_BASE + value;
