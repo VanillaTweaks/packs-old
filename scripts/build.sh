a108440133936a5592a1882f87d2bb72ced4d81a
Grant Gryczan <grantgryczan@gmail.com>
2022-03-06 00:17:02 -0500

    `rm -r` -> `rm -rf`

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
-rm -r out/*
+rm -rf out/*
 npx babel lib --out-dir out/lib --extensions .js,.ts --keep-file-extension --copy-files --source-maps &
 npx babel datapacks --out-dir out/datapacks --extensions .js,.ts --keep-file-extension --copy-files --source-maps &
 wait
--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,4 +1,4 @@
-rm -r out/*
+rm -rf out/*
 npx babel lib --out-dir out/lib --extensions .js,.ts --keep-file-extension --copy-files --source-maps --watch &
 npx babel datapacks --out-dir out/datapacks --extensions .js,.ts --keep-file-extension --copy-files --source-maps --watch &
 (sleep 3s; npx sand watch out/$1) &
