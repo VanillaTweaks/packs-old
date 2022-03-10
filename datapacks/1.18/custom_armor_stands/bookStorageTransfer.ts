c6583459b1f890d23da2efb4f854c6a511497806
Grant Gryczan <grantgryczan@gmail.com>
2022-03-10 00:49:17 -0500

    Finish `copyStorageToBook`

--- datapacks/1.18/custom_armor_stands/bookStorageTransfer.ts
+++ datapacks/1.18/custom_armor_stands/bookStorageTransfer.ts
@@ -4,18 +4,24 @@ import { data, execute, ItemModifier, MCFunction, scoreboard, tag } from 'sandst
 import { bookInMainhand, bookInOffhand } from './armorStandBook/predicates';
 import { lecternID } from './lectern';
 
-/**
- * A score with 0 if the armor stand book is in neither hand, 1 if it's in the mainhand, or 2 if it's in the offhand.
- *
- * ⚠️ The value of this score is arbitrary if the book is in a lectern.
- */
+/** A score with 0 if the armor stand book is in neither hand, 1 if it's in the mainhand, or 2 if it's in the offhand. */
 const $bookInHand = temp('$bookInHand');
 
+/** A selector to the marker with the `pack.current_lectern` tag. */
+const currentLectern = `@e[type=minecraft:marker,tag=${pack.current_lectern},limit=1]`;
+
+/**
+ * Copies the armor stand book's NBT from the lectern or the player (`@s`) to storage.
+ *
+ * ⚠️ `copyStorageToBook` must always be called within the same tick after calling this, unless the book is not found.
+ */
 export const copyBookToStorage = MCFunction(pack`_copy_book_to_storage`, () => {
+	scoreboard.players.set($bookInHand, 0);
+
 	execute
 		// TODO: Remove `as any`.
 		.if.score('@s', lecternID, 'matches', '1..' as any)
-		.run(pack`_check_lectern_for_book`, () => {
+		.run(pack`_copy_lectern_book_to_storage`, () => {
 			const $lecternID = temp('$lecternID');
 			scoreboard.players.operation($lecternID, '=', '@s', lecternID);
 
@@ -24,8 +30,6 @@ export const copyBookToStorage = MCFunction(pack`_copy_book_to_storage`, () => {
 				// TODO: Replace `'$lecternID', temp` with `$lecternID`.
 				.if.score('$lecternID', temp, '=', '@s', lecternID)
 				.run.tag('@s').add(pack.current_lectern);
-
-			const currentLectern = `@e[type=minecraft:marker,tag=${pack.current_lectern},limit=1]`;
 
 			execute
 				.at(currentLectern)
@@ -38,15 +42,14 @@ export const copyBookToStorage = MCFunction(pack`_copy_book_to_storage`, () => {
 					color: 'red'
 				});
 
-			tag(currentLectern).remove(pack.current_lectern);
+			// The `pack.current_lectern` tag is not removed here because it is used to target the same lectern again in `copyStorageToBook`.
+			// This is why `copyStorageToBook` must always be called within the same tick after calling this, to ensure that tag doesn't stay beyond this tick.
 		});
 
 	execute
 		// TODO: Remove `as any`.
 		.unless.score('@s', lecternID, 'matches', '1..' as any)
-		.run(pack`_check_hands_for_book`, () => {
-			scoreboard.players.set($bookInHand, 0);
-
+		.run(pack`_copy_hand_book_to_storage`, () => {
 			execute
 				// TODO: Remove `as any`.
 				.if.predicate(bookInMainhand as any)
@@ -75,7 +78,7 @@ export const copyBookToStorage = MCFunction(pack`_copy_book_to_storage`, () => {
 		});
 });
 
-export const copyStorageToBook = ItemModifier(pack`copy_storage_to_book`, {
+const copyStorageToBookItemModifier = ItemModifier(pack`copy_storage_to_book`, {
 	// TODO: Remove `as 'copy_nbt'`.
 	function: 'minecraft:copy_nbt' as 'copy_nbt',
 	source: {
@@ -87,4 +90,31 @@ export const copyStorageToBook = ItemModifier(pack`copy_storage_to_book`, {
 		target: '{}',
 		op: 'replace'
 	}]
+});
+
+/**
+ * Copies the armor stand book's NBT from storage back to the lectern or the player (`@s`).
+ *
+ * Does nothing if no book was found in the last call to `copyBookToStorage`.
+ *
+ * ⚠️ `copyBookToStorage` must always be called within the same tick before calling this.
+ */
+export const copyStorageToBook = MCFunction(pack`_copy_storage_to_book`, () => {
+	execute
+		.if($bookInHand.matches(0))
+		.as(currentLectern)
+		.at('@s')
+		.run(pack`_copy_storage_to_lectern_book`, () => {
+			data.modify.block('~ ~ ~', 'Book.tag').set.from.storage(pack`main`, 'book');
+
+			tag('@s').remove(pack.current_lectern);
+		});
+
+	execute
+		.if($bookInHand.matches(1))
+		.run.item.modify.entity('@s', 'weapon.mainhand', copyStorageToBookItemModifier);
+
+	execute
+		.if($bookInHand.matches(2))
+		.run.item.modify.entity('@s', 'weapon.offhand', copyStorageToBookItemModifier);
 });
