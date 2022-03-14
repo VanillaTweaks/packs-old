806f58c175c41db8cfa49fe508cb907d1df61990
Grant Gryczan <grantgryczan@gmail.com>
2022-03-13 22:41:11 -0400

    Working "Select Nearest" trigger

--- datapacks/1.18/custom_armor_stands/index.ts
+++ datapacks/1.18/custom_armor_stands/index.ts
@@ -4,9 +4,10 @@ import setMetaAdvancements from 'lib/datapacks/setMetaAdvancements';
 import setConfigFunction from 'lib/datapacks/setConfigFunction';
 import armorStandBook from './armorStandBook';
 import NBTRecipe from 'lib/datapacks/NBTRecipe';
-import { execute } from 'sandstone';
+import { execute, tag } from 'sandstone';
 import copyBookToStorage from './copy/copyBookToStorage';
 import copyStorageToBook from './copy/copyStorageToBook';
+import select from './select';
 
 setMetaAdvancements({
 	root: {
@@ -49,11 +50,29 @@ onTrigger(pack, pack.NAMESPACE, pack.TITLE, triggerObjective => {
 		// TODO: Remove `as any`.
 		.if($trigger.matches('100..' as any))
 		.run(pack`trigger/main`, () => {
+			// Tag the player so they can still be selected when executing as something else (e.g. an armor stand).
+			tag('@s').add(pack.triggerer);
+
 			copyBookToStorage();
 
+			execute
+				// TODO: Remove `as any`.
+				.if($trigger.matches('100..199' as any))
+				.run(pack`trigger/1xx`, () => {
+					execute
+						.if($trigger.matches(100))
+						.at('@s')
+						.as('@e[type=minecraft:armor_stand,distance=..24,sort=nearest,limit=1]')
+						.run(select);
 
+					execute
+						.if($trigger.matches(101))
+						.run.tellraw('@s', 'TODO: Select using mouse');
+				});
 
 			copyStorageToBook();
+
+			tag('@s').remove(pack.triggerer);
 		});
 });
 
--- datapacks/1.18/custom_armor_stands/select/index.ts
+++ datapacks/1.18/custom_armor_stands/select/index.ts
@@ -1,5 +1,8 @@
+import temp from 'lib/datapacks/temp';
 import pack from 'lib/pack';
-import { execute, MCFunction } from 'sandstone';
+import { join, minify } from 'minecraft-text-components';
+import type { JSONTextComponent } from 'sandstone';
+import { effect, execute, MCFunction, tellraw } from 'sandstone';
 import armorStandID from './armorStandID';
 
 const $lastArmorStandID = armorStandID('$last_value');
@@ -15,6 +18,34 @@ const select = MCFunction(pack`_select`, () => {
 	execute
 		.store.result.storage(pack`main`, `book.data.${pack}.armorStandID`, 'int')
 		.run.scoreboard.players.get('@s', armorStandID);
+
+	effect.give('@s', 'minecraft:glowing', 4, 0, true);
+
+	const posComponents: JSONTextComponent[] = [];
+
+	for (const posIndex of [0, 1, 2]) {
+		const $pos = temp(`$pos.${posIndex}`);
+
+		execute
+			.store.result.score($pos)
+			.run.data.get.entity('@s', `Pos[${posIndex}]`);
+
+		posComponents.push(
+			// TODO: Set `score` to `$pos` instead.
+			{ score: { name: $pos.target, objective: $pos.objective.toString() }, color: 'yellow' }
+		);
+	}
+
+	tellraw(`@a[tag=${pack.triggerer},limit=1]`, minify([
+		{ text: 'Selected the armor stand at ', color: 'gold' },
+		{ text: '(', color: 'yellow' },
+		join(
+			posComponents,
+			{ text: ', ', color: 'yellow' }
+		),
+		{ text: ')', color: 'yellow' },
+		{ text: '.', color: 'gold' }
+	]));
 });
 
 export default select;
