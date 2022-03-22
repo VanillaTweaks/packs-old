ad9c3b75f5c06da68bac400f5cd2f0d4ca77d7b6
Grant Gryczan <grantgryczan@gmail.com>
2022-03-21 23:42:37 -0400

    Make `deselect` an `MCFunction`

--- datapacks/1.18/custom_armor_stands/select/deselect.ts
+++ datapacks/1.18/custom_armor_stands/select/deselect.ts
@@ -1,13 +1,13 @@
 import pack from 'lib/pack';
-import { data } from 'sandstone';
+import { data, MCFunction } from 'sandstone';
 
 /**
  * Deselects the selected armor stand by removing the selection data from the book in storage. Does nothing if no armor stand is selected.
  *
  * It is not necessary to call this before selecting a different armor stand.
  */
-const deselect = () => {
+const deselect = MCFunction(pack`_deselect`, () => {
 	data.remove.storage(pack`main`, `book.data.${pack}.armorStandID`);
-};
+});
 
 export default deselect;
